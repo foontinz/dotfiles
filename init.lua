a17f34fbadbbda29e2e0c59914ceec8b618cb536
Dmytro Yurinov <foontinz@protonmail.com>
2025-01-23 10:09:56 +0100

    init

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("config.lazy")
+require("config.keymaps")
+vim.opt.number = true
+vim.opt.relativenumber = true
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,2 @@
+-- Telescope mappings
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Telescope Find Files' })
