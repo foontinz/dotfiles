def69cafa80d50d9bfa8a846695c72446b417034
foontinz <foontinz@protonmail.com>
2025-05-15 22:51:53 +0200

    adds zsh.sh

---
+++ cmd/zsh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install zsh
+# TODO: after everything else will be stable
+
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
